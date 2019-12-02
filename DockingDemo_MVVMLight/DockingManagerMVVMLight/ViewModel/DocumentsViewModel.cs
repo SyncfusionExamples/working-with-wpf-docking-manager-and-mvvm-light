2e1f383550ab544352fb7472e8bf27896a7a0fde
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2019-12-02 12:35:12 +0530

    Added docking manager sample for MVVMLight

---
+++ DockingDemo_MVVMLight/DockingManagerMVVMLight/ViewModel/DocumentsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using DockingAdapterMVVM;
+using Syncfusion.Windows.Shared;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockingManagerMVVMLight.ViewModel
+{
+    public class DocumentsViewModel : PaneViewModel
+    {
+        public DocumentsViewModel()
+        {
+            Header = "All Documents";
+        }
+
+        public void RaiseDocumentsPropertyChanged()
+        {
+            RaisePropertyChanged("Documents");
+        }
+
+        public IEnumerable<IDockElement> Documents
+        {
+            get
+            {
+                return from IDockElement document in MainViewModel.Workspaces
+                       where document.State == DockingAdapterMVVM.DockState.Document
+                       select document;
+            }
+        }
+
+    }
+}
---
+++ DockingDemo_MVVMLight/DockingManagerMVVMLight/ViewModel/PropertiesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockingManagerMVVMLight.ViewModel
+{
+    public class PropertiesViewModel : PaneViewModel
+    {
+        public PropertiesViewModel()
+        {
+            Header = "Properties";
+        }
+    }
+}
